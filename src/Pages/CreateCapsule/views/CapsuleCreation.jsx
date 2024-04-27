3e05e011b242829e8486a151228ece896e88e782
stefali1-dev <stefanleustean56@gmail.com>
2024-04-27 19:20:09 +0300

    create capsule form with flowbite react

--- src/Pages/CreateCapsule/views/CapsuleCreation.jsx
+++ src/Pages/CreateCapsule/views/CapsuleCreation.jsx
@@ -1,223 +1,164 @@
-import React, { useState } from "react";
+"use client";
 
-const CapsuleCreation = () => {
-  const [isGroupSelected, setIsGroupSelected] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("Select Group");
+import {
+  Label,
+  TextInput,
+  Textarea,
+  Select,
+  Radio,
+  FileInput,
+  Checkbox,
+  Button,
+} from "flowbite-react";
+import { useState, useRef } from "react";
+
+export function CapsuleCreation() {
+  const [isOrganizationSelected, setIsOrganizationSelected] = useState(false);
 
   const handleRadioChange = (event) => {
-    setIsGroupSelected(event.target.value === "Group");
-    setIsDropdownOpen(false); // Close the dropdown when changing radio button
+    setIsOrganizationSelected(event.target.value === "Organization");
   };
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+  const [selectedTheme, setSelectedTheme] = useState(null);
 
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-    setIsDropdownOpen(false); // Close the dropdown after selecting an option
+  const themes = [
+    {
+      name: "Love",
+      image: "https://source.unsplash.com/random/400x400?love", // Random image from Unsplash API
+    },
+    {
+      name: "Dark",
+      image: "https://source.unsplash.com/random/400x400?dark", // Random image from Unsplash API
+    },
+    {
+      name: "Light",
+      image: "https://source.unsplash.com/random/400x400?light", // Random image from Unsplash API
+    },
+  ];
+
+  const handleThemeSelect = (theme) => {
+    setSelectedTheme(theme);
+    // Perform any additional actions with the selected theme
   };
 
   return (
-    <form className="max-w-sm mx-auto">
-      <div class="mb-5">
-        <label
-          for="base-input"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Capsule Name
-        </label>
-        <input
-          type="text"
-          id="base-input"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
+    <div className="flex max-w-md flex-col gap-4 mx-auto">
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="base" value="Capsule Name" />
+        </div>
+        <TextInput id="base" type="text" sizing="md" />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="large" value="Capsule Description" />
+        </div>
+        <TextInput id="large" type="text" sizing="lg" />
       </div>
 
-      <div class="mb-5">
-        <label
-          for="large-input"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Capsule Description
-        </label>
-        <input
-          type="text"
-          id="large-input"
-          class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
+      <div className="max-w-md">
+        <div className="mb-2 block">
+          <Label htmlFor="comment" value="Capsule Text Content" />
+        </div>
+        <Textarea id="comment" placeholder="Type..." required rows={4} />
       </div>
 
       <div>
-        <fieldset>
-          <legend className="sr-only">Create Capsule as:</legend>
-          <div className="flex items-center mb-4">
-            <input
-              id="radio-person-option"
-              type="radio"
+        <fieldset className="flex max-w-md flex-col gap-4">
+          <legend className="mb-4">Create Capsule as:</legend>
+          <div className="flex items-center gap-2">
+            <Radio
+              id="person"
               name="posting"
               value="Person"
-              className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
-              checked={!isGroupSelected}
+              defaultChecked
               onChange={handleRadioChange}
             />
-            <label
-              htmlFor="radio-person-option"
-              className="block ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Person
-            </label>
+            <Label htmlFor="Person">Person</Label>
           </div>
-          <div className="flex items-center mb-4">
-            <input
-              id="radio-group-option"
-              type="radio"
+          <div className="flex items-center gap-2">
+            <Radio
+              id="organization"
               name="posting"
-              value="Group"
-              className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600"
-              checked={isGroupSelected}
+              value="Organization"
               onChange={handleRadioChange}
             />
-            <label
-              htmlFor="radio-group-option"
-              className="block ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Group
-            </label>
+            <Label htmlFor="Organization">Organization</Label>
           </div>
         </fieldset>
 
-        {isGroupSelected && (
-          <div>
-            <button
-              id="dropdownDefaultButton"
-              data-dropdown-toggle="dropdown"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              type="button"
-              onClick={toggleDropdown}
-            >
-              {selectedOption}
-              <svg
-                className="w-2.5 h-2.5 ms-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </button>
-
-            <div
-              id="dropdown"
-              className={`z-10 ${
-                isDropdownOpen ? "block" : "hidden"
-              } bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
-            >
-              <ul
-                className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdownDefaultButton"
-              >
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    onClick={() => handleOptionClick("Centric")}
-                  >
-                    Centric
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    onClick={() => handleOptionClick("Fii Uaic")}
-                  >
-                    Fii Uaic
-                  </a>
-                </li>
-              </ul>
+        {isOrganizationSelected && (
+          <div className="max-w-md mt-4">
+            <div className="mb-2 block">
+              <Label htmlFor="organisations" value="Select your organization" />
             </div>
+            <Select id="organisations" required>
+              <option>Centric</option>
+              <option>FII Uaic</option>
+            </Select>
           </div>
         )}
       </div>
 
-      <form class="max-w-sm mx-auto">
-        <label
-          for="message"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Capsule Text Content
-        </label>
-        <textarea
-          id="message"
-          rows="4"
-          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Type..."
-        ></textarea>
-      </form>
-
-      <form class="max-w-lg mx-auto">
-        <label
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          for="user_avatar"
-        >
-          Upload file
-        </label>
-        <input
-          class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-          aria-describedby="user_avatar_help"
-          id="user_avatar"
-          type="file"
+      <div id="fileUpload" className="max-w-md">
+        <div className="mb-2 block">
+          <Label htmlFor="file" value="Upload file" />
+        </div>
+        <FileInput
+          id="file"
+          helperText="Accepted file formats: .jpg, .png, .mp3, .mp4"
         />
-        <div
-          class="mt-1 text-sm text-gray-500 dark:text-gray-300"
-          id="user_avatar_help"
-        >
-          Supported files: .jpg, .png, .mp3, .mp4
-        </div>
-      </form>
-
-      <div className="flex items-start mb-5">
-        <div className="flex items-center h-5">
-          <input
-            id="terms"
-            type="checkbox"
-            value=""
-            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-            required
-          />
-        </div>
-
-        <label
-          htmlFor="terms"
-          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          I agree with the{" "}
-          <a
-            href="#"
-            className="text-blue-600 hover:underline dark:text-blue-500"
-          >
-            terms and conditions
-          </a>
-        </label>
       </div>
 
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Create Capsule
-      </button>
-    </form>
+      <div className="max-w-md mx-auto mt-8">
+        <h2 className="text-2xl font-bold mb-4">Select a Theme</h2>
+        <ul className="divide-y divide-gray-200">
+          {themes.map((theme, index) => (
+            <li
+              key={index}
+              className={`py-4 flex items-center justify-between hover:bg-gray-100 cursor-pointer ${
+                selectedTheme === theme ? "bg-indigo-100" : ""
+              }`}
+              onClick={() => handleThemeSelect(theme)}
+            >
+              <div className="flex items-center">
+                <img
+                  src={theme.image}
+                  alt={theme.name}
+                  className="w-16 h-16 rounded-full object-cover mr-4"
+                />
+                <span className="text-lg font-semibold">{theme.name}</span>
+              </div>
+              {selectedTheme === theme && (
+                <span className="text-indigo-600">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                </span>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Checkbox id="remember" />
+        <Label htmlFor="remember">Terms & Conditions</Label>
+      </div>
+      <Button type="submit">Create Capsule</Button>
+    </div>
   );
-};
+}
 
 export default CapsuleCreation;
